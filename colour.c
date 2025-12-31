701806818d09422af76b736d39a0569556854e59
tharnyelilvannan <tharny2005@outlook.com>
2025-12-30 19:26:23 -0500

    Added tests (passed)

--- colour.c
+++ colour.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 #include "colour.h"
 
 int main(int argc, char *argv[])
@@ -9,7 +10,7 @@ int main(int argc, char *argv[])
     {
         printf("Incorrect number of arguments.\n");
         printf("--help for more information.\n");
-        return 1;
+        exit(1);
     }
 
     if (strcmp(argv[1], "--help") == 0)
@@ -22,17 +23,38 @@ int main(int argc, char *argv[])
         if (strcmp(argv[3], "-rgb") == 0)
         {
             char *hex = argv[2];
+
+            regex_t regex;
+
+            int valid = regcomp(&regex, "([0-9]|[A-F]){6}", REG_EXTENDED);
+
+            if (valid)
+            {
+                printf("Could not compile regex.\n");
+                exit(6);
+            }
+
+            valid = regexec(&regex, hex, 0, NULL, 0);
+
+            if (valid)
+            {
+                printf("Invalid hex code.");
+                exit(5);
+            }
+
+            regfree(&regex);
+
             int err = hex_to_rgb(hex);
 
             if (err != 0)
             {
                 printf("Error converting to RGB value.\n");
-                return 3;
+                exit(3);
             }
         }
         else
         {
-            return 2;
+            exit(2);
         }
     }
     else if (strcmp(argv[1], "-rgb") == 0)
@@ -49,25 +71,31 @@ int main(int argc, char *argv[])
                 long int g = strtol(argv[3], &g_end, 10);
                 long int b = strtol(argv[4], &b_end, 10);
 
+                if (r > 255 || r < 0 || g > 255 || g < 0 || b > 255 || b < 0)
+                {
+                    printf("Invalid RGB code.");
+                    exit(5);
+                }
+
                 int err = rgb_to_hex(r, g, b);
 
                 if (err != 0)
                 {
                     printf("Error converting to hex value.\n");
-                    return 3;
+                    exit(3);
                 }
             }
             else
             {
-                return 2;
+                exit(2);
             }
         }
         else
         {
             printf("Incorrect number of arguments.\n");
             help();
-            return 1;
+            exit(1);
         }
-        return 0;
+        exit(0);
     }
 }
